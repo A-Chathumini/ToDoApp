cf83153223d86df1e3501548815e9a8178a5a441
A-Chathumini <ackatuwawala@gmail.com>
2020-03-28 21:27:28 +0530

    deployed my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './Todos'
+import AddTodo from './AddTodo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state={
+    todos:[
+      {id:1,content:'buy some milk'},
+      {id:2,content:'Reading books'}
+    ]
+  }
+  deleteTodo=(id)=> {
+    const todos=this.state.todos.filter(todo =>{
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+    })
+  }
+  addTodo=(todo)=>{
+     todo.id=Math.random();
+     let todos=[...this.state.todos,todo];
+     this.setState({
+       todos:todos
+     })
+  }
+  render(){
+    return(
+      <div className="todo-app container">
+      <h1 className="center blue-text">Todos</h1>
+      <p>Type a todo and press enter to add to the list.Click on a list item to delete.</p>
+
+      <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+      <AddTodo addTodo={this.addTodo}/>
+
+      </div>
+    );
+  }
 }
 
 export default App;
