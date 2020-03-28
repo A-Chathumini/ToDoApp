646cf05bf31ad203378b6021f9c9443e5d5cb8a1
A-Chathumini <ackatuwawala@gmail.com>
2020-03-28 23:19:06 +0530

    Updates

---
+++ precache-manifest.99b45af5bb3ae8e7bdb2dc861abf4c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9efd799f64906c1e4efd17a2815a3610",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "b3e55bd7af097fb8dfb8",
+    "url": "/ToDoApp/static/css/main.38d72215.chunk.css"
+  },
+  {
+    "revision": "428664c9a7749960040e",
+    "url": "/ToDoApp/static/js/2.e8d11de2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoApp/static/js/2.e8d11de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3e55bd7af097fb8dfb8",
+    "url": "/ToDoApp/static/js/main.b2e8b3be.chunk.js"
+  },
+  {
+    "revision": "8d4ab1033eb9486c58ae",
+    "url": "/ToDoApp/static/js/runtime-main.68f74e45.js"
+  }
+]);
